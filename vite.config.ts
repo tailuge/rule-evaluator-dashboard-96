74a7ce7edfe673913b604919afe2e36146f83934
tailuge <tailuge@gmail.com>
2025-02-27 19:27:16 +0000

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "./" : "/rule-evaluator-dashboard-96/", // Ensure correct base path
+  base: "./", 
   server: {
     host: "::",
     port: 8080,
   },
   build: {
-    outDir: "docs", // Output the build to /docs instead of /dist
+    outDir: "docs", 
   },
   plugins: [
     react(),
